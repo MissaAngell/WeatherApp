80e15c50d682d61617773cd86e47b44c24fbdd79
Marissa <mangell@madisoncollege.edu>
2019-11-25 16:05:13 -0600

    work done on Nov 25

--- weather.js
+++ weather.js
@@ -4,24 +4,54 @@
 
 //api key e73aeba8adce70bbc2ab74491f6d4923
 
-$("#weather_btn").click(function(){
-    
+$("#weather_btn").click(function () {
+
     //variables for the Open Weather API URL
     var apiLink = 'https://api.openweathermap.org/data/2.5/weather';
     var apiKey = 'e73aeba8adce70bbc2ab74491f6d4923';
+    var apiUnits = '&units=imperial';
     var zipInput = $('#zipCode').val();
-    var apiURL = apiLink + '?zip=' + zipInput + '&appid=' + apiKey;
+    var apiURL = apiLink + '?zip=' + zipInput + '&appid=' + apiKey + apiUnits;
     console.log(apiURL);
-    
-    $.get(apiURL, function(responseFromOW){
-            console.log(responseFromOW);
-            console.log("temp:", responseFromOW.main.temp);
-            console.log("city:", responseFromOW.name);
-            console.log("condition:", responseFromOW.weather[0].description);
-        
-            var temp = responseFromOW.main.temp;
-            var city = responseFromOW.name;
-            var condition = responseFromOW.weather[0].description;
+
+    $.get(apiURL, function (responseFromOW) {
+        console.log(responseFromOW);
+        console.log("temp:", responseFromOW.main.temp);
+        console.log("city:", responseFromOW.name);
+        console.log("condition:", responseFromOW.weather[0].main);
+
+        var temp = responseFromOW.main.temp;
+        var city = responseFromOW.name;
+        var condition = responseFromOW.weather[0].main;
+
+        $('#city').html(city);
+        $('#degrees').html(temp);
+        $('#typeweather').html(condition);
+
+
+
+
+        if (responseFromOW.weather[0].main === 'Clouds') {
+            $('#picture').css('background-image', 'url(img/cloudy.jpeg)');
+        } else if (responseFromOW.weather[0].main === 'Clear') {
+            $('#picture').css('background-image', 'url(img/clear.jpg)');
+        } else if (responseFromOW.weather[0].main === 'Tornado') {
+            $('#picture').css('background-image', 'url(img/tornado.jpg)');
+        } else if (responseFromOW.weather[0].main === 'Snow'){
+            $('#picture').css('background-image', 'url(img/snow.jpg)');
+        } else if (responseFromOW.weather[0].main === 'Rain'){
+            $('#picture').css('background-image', 'url(img/rain.jpg)');
+        } else if (responseFromOW.weather[0].main === 'Thunderstorm'){
+            $('#picture').css('background-image', 'url(img/thunderstorm.jpg)');
+        } else if (responseFromOW.weather[0].main === 'Mist'){
+            $('#picture').css('background-image', 'url(img/mist.jpg)');
+        } else if (responseFromOW.weather[0].main === 'Fog'){
+            $('#picture').css('background-image', 'url(img/fog.jpg)');
+        } else if (responseFromOW.weather[0].main === 'Drizzle'){
+            $('#picture').css('background-image', 'url(img/drizzle.jpg)');
+        }
     });
-    
+
+
+
 });
